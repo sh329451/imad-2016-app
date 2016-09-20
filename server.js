2ed654656bce0fab3690cca69b1a5023a77e8775
sh329451 <nissh.nic@gmail.com>
2016-09-20 20:05:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ var articles = {
 },
 };
 function createTemplate(data){
-var title1 = data.title1;
+var title1 = data.title;
 var heading = data.heading;
 var date = data.date;
 var content = data.content;
